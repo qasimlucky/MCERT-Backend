df56f821c7941f9b93f6a2e5a8a72b522103e179
Qasim Ali <raorizwan@gmail.com>
2025-08-23 14:04:56 +0500

    thid id cors

--- src/main.ts
+++ src/main.ts
@@ -12,7 +12,7 @@ async function bootstrap() {
   
   // Enable CORS
   app.enableCors({
-    origin: ['http://localhost:3001', 'https://sirisreports.co.uk', 'https://sirisreports.xyz','http://localhost:3002'],
+    origin: ['http://localhost:3001', 'https://sirisreports.co.uk', 'https://sirisreports.xyz','http://localhost:3002',"https://mcert-frontend.vercel.app/"],
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
     allowedHeaders: [
