49ae7ef297edf229417e6559816f0573681d225a
neiakgna <raorizwan@gmail.com>
2025-09-18 01:15:39 +0500

    this is setting

--- src/forms/dto/update-form.dto.ts
+++ src/forms/dto/update-form.dto.ts
@@ -1,4 +1,17 @@
 import { PartialType } from '@nestjs/mapped-types';
+import { IsString, IsBoolean, IsNumber, IsOptional } from 'class-validator';
 import { CreateFormDto } from './create-form.dto';
 
-export class UpdateFormDto extends PartialType(CreateFormDto) {} 
+export class UpdateFormDto extends PartialType(CreateFormDto) {
+  @IsString()
+  @IsOptional()
+  gridFSFileId?: string;
+
+  @IsBoolean()
+  @IsOptional()
+  isLargeData?: boolean;
+
+  @IsNumber()
+  @IsOptional()
+  dataSize?: number;
+} 
